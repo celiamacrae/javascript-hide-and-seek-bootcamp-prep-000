e6b83539bef09bf952bd3bcd7ca939dec486fe84
amazing-server-2710 <celiamacrae@gmail.com>
2019-07-17 16:44:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+httpserver
+
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector(.target);
+}
+
 
