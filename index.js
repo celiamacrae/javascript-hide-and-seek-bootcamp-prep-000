7b098f55a4e84bcbe5f611c8d7f25623c05e96f3
amazing-server-2710 <celiamacrae@gmail.com>
2019-07-17 17:02:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function increaseRankBy(n){
 function deepestChild(){
   var gn = document.querySelector('#grand-node');
   var deepest = gn.children[0];
-  for(var i=0; deepest.children[i]=== null; i){
+  for(var i=0; !deepest.children; i){
     deepest = deepest.children[0];
   }
   return deepest;
