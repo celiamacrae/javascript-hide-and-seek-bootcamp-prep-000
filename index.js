fcca4410119e6c91ba496241e54bac2e7a9a92fc
amazing-server-2710 <celiamacrae@gmail.com>
2019-07-17 17:06:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+httpserver
+
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  var rankings = document.querySelectorAll('ul.ranked-list li');
+  for(var i=0; i< rankings.length; i++){
+    rankings[i].innerHTML = parseInt(rankings[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  var deepest = deep[deep.length-1]
+  return deepest
+}
 
